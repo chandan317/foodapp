d7243bd8cfedeff77b53deddaa3561d1f31f8761
chandantm317 <chandantmchandu31@gmail.com>
2023-03-09 16:10:23 +0530

    chanfe f done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
+import { useEffect, useState } from "react";
+import SearchBar from "./components/SearchBar";
+import RecipeCard from "./components/RecipeCard";
 
-import './App.css';
+import "./App.css";
+
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  //function fetch data from api
+  //i.e function to search
+
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = apiUrl + query;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searchRecipes([]);
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchRecipes();
+  };
+
   return (
-    <div className="App">
-   <h1> hiiii</h1>
+    <div className="container">
+      <h1> Food Recipe App</h1>
+      <SearchBar
+        handleSubmit={handleSubmit}
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        isLoading={isLoading}
+      />
+
+      <div className="recipes">
+        {recipes
+          ? recipes.map((recipe) => (
+              <RecipeCard key={recipe.idMeal} recipe={recipe} />
+            ))
+          : "No Recipes"}
+      </div>
     </div>
   );
 }
